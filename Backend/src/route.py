8dd5ec8f5ff43b934a7d82a82790d206c4bec3dd
Min <200134kms@gmail.com>
2023-10-05 03:15:11 +1100

    added the types of google api and made assumption for each places. finished basic fetching for getPlaces function. Structured the route.py

--- Backend/src/route.py
+++ Backend/src/route.py
@@ -1,81 +1,280 @@
-import googlemaps
-from datetime import datetime, timedelta
 import os
+import json
+import requests
+import random
 
-file_path = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'api_key.txt')
 
-with open(file_path, 'r') as file:
-    apiKey = file.read()
-    # print(apiKey)
-    gmaps = googlemaps.Client(key=apiKey)
+TYPES = {
+    "amusement_park": {
+        "duration": 5,
+        "eat": False,
+        "day": True
+    },
+    "aquarium": {
+        "duration": 2.5,
+        "eat": False,
+        "day": True
+    },
+    "art_gallery": {
+        "duration": 1.5,
+        "eat": False,
+        "day": True
+    },
+    "bakery": {
+        "duration": 0.75,
+        "eat": True,
+        "day": True
+    },
+    "bar": {
+        "duration": 2.5,
+        "eat": True,
+        "day": False
+    },
+    "bowling_alley": {
+        "duration": 1.5,
+        "eat": False,
+        "day": True
+    },
+    "cafe": {
+        "duration": 1.5,
+        "eat": True,
+        "day": True
+    },
+    "casino": {
+        "duration": 2.5,
+        "eat": False,
+        "day": False
+    },
+    "movie_theater": {
+        "duration": 2.5,
+        "eat": False,
+        "day": True
+    },
+    "museum": {
+        "duration": 3,
+        "eat": False,
+        "day": True
+    },
+    "night_club": {
+        "duration": 4,
+        "eat": False,
+        "day": False
+    },
+    "park": {
+        "duration": 2,
+        "eat": False,
+        "day": True
+    },
+    "shopping_mall": {
+        "duration": 3,
+        "eat": False,
+        "day": True
+    },
+    "zoo": {
+        "duration": 4,
+        "eat": False,
+        "day": True
+    },
+    "tourist_attraction": {
+        "duration": 2.5,
+        "eat": False
+    },
+    "restaurant": {
+        "duration": 1.5,
+        "eat": True
+    }
+}
 
-# SYDNEY_ADDRESS = ', Sydney, NSW, Australia'
+SOLO_TYPES = [
+    "art_gallery",
+    "bakery",
+    "bowling_alley",
+    "cafe",
+    "movie_theater",
+    "museum",
+    "park",
+    "shopping_mall",
+    "zoo"
+]
 
-# def toSydneyAddress(address):
-#     '''
-#     input: address(string)
-#     output: address(string)
-#     '''
-#     return address+SYDNEY_ADDRESS
+COUPLE_TYPES = [
+    "amusement_park",
+    "aquarium",
+    "art_gallery",
+    "bar",
+    "bakery",
+    "movie_theater",
+    "museum",
+    "park",
+    "shopping_mall",
+    "zoo"
+]
 
-# def getPostcode(address):
-#     result = None
-#     geocode_result = gmaps.geocode(toSydneyAddress(address), region='AU')
-#     #print(geocode_result)
-#     # Extract the postcode from the Geocoding result
-#     if geocode_result:
-#         for component in geocode_result[0]['address_components']:
-#             if 'postal_code' in component['types']:
-#                 result = component['long_name']
-#                 break
-#     return result
+FAMILY_TYPES = [
+    "amusement_park",
+    "aquarium",
+    "art_gallery",
+    "bakery",
+    "cafe",
+    "movie_theater",
+    "museum",
+    "park",
+    "shopping_mall",
+    "zoo"
+]
 
-# def getCoordinates(address):
-#     '''
-#     input: address(string)
-#     output: tuple(latitude, longtidue)
-#     '''
-#     result = None
-#     geocode_result = gmaps.geocode(toSydneyAddress(address))
-#     if geocode_result:
-#         address = geocode_result[0]['geometry']['location']
-#         result = address['lat'], address['lng']
-#     return result
+FRIENDS_TYPES = [
+    "amusement_park",
+    "aquarium",
+    "art_gallery",
+    "bar",
+    "bowling_alley",
+    "cafe",
+    "casino",
+    "movie_theater",
+    "museum",
+    "night_club",
+    "park",
+    "shopping_mall",
+    "zoo"
+]
 
-# def getInfraInTheRange(infra, range, coordinate):
-#     '''
-#     input: infra(string), range in meter(float), tuple(latitude, longtidue)
-#     output: boolean
-#     '''
-#     result = None
-#     infras = gmaps.places_nearby(location=(coordinate[0], coordinate[1]), radius=range, type=infra)['results']   
-#     result = len(infras) > 0
-#     return result
 
-# def getTravelTime(departure, destination):
-#     '''
-#     input: tuple(latitude, longtidue), tuple(latitude, longtidue)
-#     output: integer(mins)
-#     '''
-#     DEPARTURE_TIME = datetime(2023, 6, 26, 12, 00, 0)
-#     result = None
-#     directions_result = []
-#     while len(directions_result) == 0:
-#         directions_result = gmaps.directions(departure, destination, departure_time=DEPARTURE_TIME, mode="transit")
-#         DEPARTURE_TIME = DEPARTURE_TIME - timedelta(days=1)
-#     time_string = directions_result[0]['legs'][0]['duration']['text']
-#     time_parts = time_string.split()
-#     if len(time_parts) == 4:
-#         result = int(time_parts[0]) * 60 + int(time_parts[2])
-#     elif len(time_parts) == 2:
-#         result = int(time_parts[0])
-#     else:
-#         result = None
-#     return result
+NEARBYSEARCH_URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
 
-# #print(getInfraInTheRange('zoo', 2500, getCoordinates('Kensington , Sydney, Australia')))
-# #print(getTravelTime(getCoordinates('Abbotsford'), getCoordinates('Abbotsbury')))
-# #print(getCoordinates('Sydney , Sydney, Australia'))
-# #print(getCoordinates('Kensington , Sydney, Australia'))
 
-# #print(getCoordinates('2012'))
+def getApi():
+    file_path = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'api_key.txt')
+
+    with open(file_path, 'r') as file:
+        return file.read()
+
+#fixme: need to change the request format as getPlaces function
+def getRoutes(API, origin, destination):
+    
+    headers = {
+        'Content-Type': 'application/json',
+        'X-Goog-Api-Key': API,
+        'X-Goog-FieldMask': 'routes.legs.steps.transitDetails'
+    }
+
+    data = {
+        "origin": {
+            "address": origin
+        },
+        "destination": {
+            "address": destination
+        },
+        "travelMode": "TRANSIT",
+        "computeAlternativeRoutes": True,
+        "transitPreferences": {
+            "routingPreference": "LESS_WALKING",
+            "allowedTravelModes": ["TRAIN"]
+        }
+    }
+
+    response = requests.post('https://routes.googleapis.com/directions/v2:computeRoutes', headers=headers, data=json.dumps(data))
+    return response
+
+
+def getPlaces(API, template, location, distance, budget):
+
+    activities = []
+
+    if template == "solo":
+        random.shuffle(SOLO_TYPES)
+        types = SOLO_TYPES[0:3]
+    elif template == "couple":
+        random.shuffle(COUPLE_TYPES)
+        types = COUPLE_TYPES[0:3]
+    elif template == "family":
+        random.shuffle(FAMILY_TYPES)
+        types = FAMILY_TYPES[0:3]
+    elif template == "friends":
+        random.shuffle(FRIENDS_TYPES)
+        types = FRIENDS_TYPES[0:3]
+
+    print(types)
+
+    for i in types:
+        params = {
+            "location": location,
+            "radius": distance,
+            "maxprice": budget,
+            "type": i,
+            "key": API
+        }
+
+        response = requests.get(NEARBYSEARCH_URL, params=params)
+
+        if response.status_code == 200:
+            activities+=response.json().get("results", [])
+        else:
+            print(f"Error {response.status_code}: {response.text}")
+        
+    params = {
+        "location": location,
+        "radius": distance,
+        "maxprice": budget,
+        "type": 'restaurant',
+        "key": API
+    }
+
+    response = requests.get(NEARBYSEARCH_URL, params=params)
+
+    if response.status_code == 200:
+        restaurantes=response.json().get("results", [])
+    else:
+        print(f"Error {response.status_code}: {response.text}")
+
+    return {"restaurantes": restaurantes, "activities": activities}
+
+
+def getShortestRoute():
+    pass
+
+
+def getQualitativeRoute():
+    pass
+
+
+def getNoMealRoute():
+    pass
+
+
+def getLocalSpecialtyRoute():
+    pass
+
+
+def getAllRoutes(location, distance, time, duration, transportation, budget, template):
+    API = getApi()
+
+    LOCATION = location
+    DISTANCE = distance #maximum 5000(m)
+    TIME = time
+    DURATION = duration
+    TRANSPORTATION = transportation
+    BUDGET = budget #between 0(free) ~ 4(Expensive)
+    TEMPLATE = template
+
+    return{
+        "shortest": getShortestRoute(),
+        "qualitative": getQualitativeRoute(),
+        "noMeal": getNoMealRoute(),
+        "localSpecialty": getLocalSpecialtyRoute(),
+    }
+
+
+
+location = '-33.870396, 151.207030'
+distance = '1000'
+time = ''
+duration = ''
+transportation = ''
+budget = '4'
+template = 'solo'
+
+
+# print(getPlaces(getApi(), template, location, distance, budget))
+
+# print(getAllRoutes(location, distance, time, duration, transportation, budget, template))

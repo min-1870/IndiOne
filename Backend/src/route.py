fc3a80b181f7f0c3c8ae9f87eda433d1b19de1e9
Min <200134kms@gmail.com>
2023-10-07 01:28:44 +1100

    finished 3 funcitons for generation algorithm

--- Backend/src/route.py
+++ Backend/src/route.py
@@ -143,6 +143,11 @@ MEAL_TIMES = [
         {"startTime":18, "endTime": 21},
 ]
 
+TRAVEL_TIMES = {
+    'walking':0.2,
+    'public':0.075,
+    'private':0.05
+}
 
 #========================================================================fetching functions
 
@@ -178,30 +183,31 @@ def calculateDistance(location1, location2):
     return distance * 1000
 
 
-def getActivities(template, location, distance, budget):
-    if template == "solo":
+def getActivities(userInput):
+    
+    if userInput['template'] == "solo":
         random.shuffle(SOLO_TYPES)
         types = SOLO_TYPES[0:3]
-    elif template == "couple":
+    elif userInput['template'] == "couple":
         random.shuffle(COUPLE_TYPES)
         types = COUPLE_TYPES[0:3]
-    elif template == "family":
+    elif userInput['template'] == "family":
         random.shuffle(FAMILY_TYPES)
         types = FAMILY_TYPES[0:3]
-    elif template == "friends":
+    elif userInput['template'] == "friends":
         random.shuffle(FRIENDS_TYPES)
         types = FRIENDS_TYPES[0:3]
 
     activites = []
 
     for type_ in types:
-        activites += refineResult(type_, googleMapsApi.getPlaces(type_, location, distance, budget))
+        activites += refineResult(type_, googleMapsApi.getPlaces(type_, userInput['location'], userInput['distance'], userInput['budget']))
 
     return activites
 
 
-def getRestaurants(location, distance, budget):
-    activites = googleMapsApi.getPlaces('restaurant', location, distance, budget)
+def getRestaurants(userInput):
+    activites = googleMapsApi.getPlaces('restaurant', userInput['location'], userInput['distance'], userInput['budget'])
 
     return refineResult('restaurant', activites)
 
@@ -209,72 +215,146 @@ def getRestaurants(location, distance, budget):
 #========================================================================place selector functions
 
 
-def getClosestPlace(location, places):
-    pass
+def getClosestPlace(userInput, currentLocation, places):
+    closest = ''
+    distance = float('inf')
+    for place in places:
+        newDistance = calculateDistance(currentLocation, place['geometry']['location'])
+        if newDistance < distance:
+            distance = newDistance
+            closest = place
 
-def getQualitativePlace(location, places):
-    pass
+    return {'place':closest, 'travelTime':TRAVEL_TIMES[userInput['transportation']]*distance}
 
-def getRandomPlace(location, places):
-    pass
+
+def getQualitativePlace(userInput, currentLocation, places):
+    best = ''
+    rating = 0
+    for place in places:
+        if rating < place['rating']:
+            rating = place['rating']
+            best = place
+            
+            if rating == 5.0:
+                break
+
+    travelTime = TRAVEL_TIMES[userInput['transportation']]*calculateDistance(currentLocation, best['geometry']['location'])
+    
+    return {'place':best, 'travelTime':travelTime}
+
+
+# def getRandomPlace(userInput, places):
+#     return places[random.randrange(0, len(places))]
 
 
 #========================================================================generative algorithm
 
 
-def generateRoute(startTime, endTime, location, activities, nextPlaceSelector, restaurants=None):
+def generateRoute(userInput, nextPlaceSelector, places):
     
     lastMealTime = None
-    currentLocation = location
-    currentTime = startTime
+    activities = places['activities']
+    restaurants = places['restaurants']
+    currentLocation = userInput['location']
+    currentTime = userInput['time']
+    endTime = userInput['time'] + userInput['duration']
     route = []
     
     while currentTime < endTime:
         if (restaurants != None) and (4 <= currentTime - lastMealTime):
             if 0 != len(list(filter(lambda mealTime: mealTime['startTime'] < currentTime < mealTime['endTime'], MEAL_TIMES))):
                 #select next place
-                nextPlace = nextPlaceSelector(currentLocation, restaurants)
+                nextPlace = nextPlaceSelector(userInput, currentLocation, restaurants)
+
+                #place
+                place = nextPlace['place']
+
+                #travel time
+                travelTime = nextPlace['travelTime']
                 
                 #remove the selected place
-                restaurants = [restaurant for restaurant in restaurants if nextPlace['place_id'] != restaurant['place_id']]
+                restaurants = [restaurant for restaurant in restaurants if place['place_id'] != restaurant['place_id']]
                 
                 #calculate time
-                currentTime += TYPES[nextPlace['type']]["duration"]
+                currentTime += TYPES[place['type']]["duration"]
+                currentTime += travelTime
                 lastMealTime = currentTime
                 
 
         else:
             #select next place
-            nextPlace = nextPlaceSelector(currentLocation, activities)
+            nextPlace = nextPlaceSelector(userInput, currentLocation, activities)
+
+            #place
+            place = nextPlace['place']
+
+            #travel time
+            travelTime = nextPlace['travelTime']
             
             #remove the selected place
-            activities = [activity for activity in activities if nextPlace['place_id'] != activity['place_id']]
+            activities = [activity for activity in activities if place['place_id'] != activity['place_id']]
             
             #calcualte time
-            currentTime += TYPES[nextPlace['type']]["duration"]
+            currentTime += TYPES[place['type']]["duration"]
         
+        route.append({'type':'travelTime', 'travelTime':travelTime})
+
         #update new place to the route
-        route.append(nextPlace)
+        route.append(place)
 
         #update current location
-        currentLocation = nextPlace['geometry']['location']
+        currentLocation = place['geometry']['location']
+
+    return {'route':route, 'restPlaces':{'activities':activities, 'restaurants':restaurants} }
 
 
 #generate initial population with route selectors
-def generateFirstPopulation(populationNumber):
-    pass
+def generateFirstPopulation(userInput, populationNumber, places):
+    routes = []
+    placesC = places[:]
+    placesQ = places[:]
 
-#O(log(n)/2) removing the worst route untill 50% left
-def removeBadPopulation(population):
-    pass
+    for _ in range(populationNumber):
+        newRoute = generateRoute(userInput, getClosestPlace, placesC)
+        routes.append(newRoute['route'])
+        placesC = newRoute['restPlaces']
 
+    for _ in range(populationNumber):
+        newRoute = generateRoute(userInput, getQualitativePlace, placesQ)
+        routes.append(newRoute['route'])
+        placesC = newRoute['restPlaces']
+
+    return routes
+    
+
+#todo: finish this function
 #make the population double with some mutation
 def generateNextPopulation(population, mutationRate):
     pass
 
-def runPopulation(intialPopulationNumber, generationNumber, mutationRate):
+
+#todo: finish this function
+#O(log(n)/2) removing the worst route untill 50% left
+# -travelTime, +diversity, +rating, +user_ratings_total*0.5, 
+def removeBadPopulation(population):
+    for _ in range(int(len(population)/2)):
+        for route in population:
+            travelTime = 0
+            types = []
+            rating = 0
+            user_ratings_total = 0
+            for place in route:
+                if place['type'] == "travelTime":
+                    travelTime += place['travelTime']
+                else:
+                    types.append(place['type'])
+                    rating += float(place['rating'])
+                    user_ratings_total += int(place['user_ratings_total'])
+
+
+def runPopulation(userInput, places, intialPopulationNumber, generationNumber, mutationRate):
     
-    population = generateFirstPopulation(intialPopulationNumber)
+    population = generateFirstPopulation(userInput, intialPopulationNumber, places)
     for _ in generationNumber:
         population = generateNextPopulation(population, mutationRate)
         population = removeBadPopulation(population)
@@ -285,11 +365,15 @@ def runPopulation(intialPopulationNumber, generationNumber, mutationRate):
 
 
 
-location = '-33.917663, 151.232869'
-distance = '1000'
-time = ''
-duration = ''
-transportation = ''
-budget = '2'
-template = 'solo'
+
+
+userInput = {
+    'location': {'lat':-33.917663, 'lng':151.232869},
+    'distance': 1000,
+    'time': 5,
+    'duration': 7,
+    'transportation': 'public',
+    'budget': 2,
+    'template':'solo',
+}
 print(getActivities(template, location, distance, budget))

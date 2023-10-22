1d061dab98849a6b49e9c44c1f87b2b294ca972b
Siwoo Jung <142607954+siwoo-jung@users.noreply.github.com>
2023-10-23 10:08:22 +1100

    commited changes

--- Frontend/google_maps_tutorial/src/App.tsx
+++ Frontend/google_maps_tutorial/src/App.tsx
@@ -1,12 +1,33 @@
 import "./App.css";
-import SampleMap from "./components/SampleMap";
+import BudgetRange from "./components/Budget";
+import DateForm from "./components/DateForm";
+import Transportation from "./components/Transportation";
+import "bootstrap/dist/css/bootstrap.css";
+import StartLocation from "./components/StarLocation";
+import Duration from "./components/Duration";
+import UserInput from "./components/UserInput";
 
 function App() {
   return (
     <>
-      <div>THIS IS REACT APP</div>
-      <div className="box">
+      <div>
+        <UserInput></UserInput>
+      </div>
+      <div>
+        <Transportation></Transportation>
+      </div>
+
+      <div className="budget_range_container">
+        <BudgetRange></BudgetRange>
+      </div>
+
+      <DateForm></DateForm>
+      {/* <StartLocation></StartLocation> */}
+      {/* <div id="appMap">
         <SampleMap></SampleMap>
+      </div> */}
+      <div className="budget_range_container">
+        <Duration></Duration>
       </div>
     </>
   );
--- Frontend/google_maps_tutorial/vite.config.ts
+++ Frontend/google_maps_tutorial/vite.config.ts
@@ -2,15 +2,15 @@ import { defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-export default defineConfig(({ mode }) => {
-  plugins: [react()]
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
   return {
-    define: {
-      'process.env.VITE_PUBLIC_GOOGLE_MAPS_API_KEY': JSON.stringify(mode),
-    },
-  }
-})
+      plugins: [react()],
+      server: {
+        open: true,
+      },
+      define: {
+          VITE_API_KEY: JSON.stringify(env.VITE_API_KEY),
+      },
+  };
+});
